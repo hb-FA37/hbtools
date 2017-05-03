898220430aa07ae529fbe5e81a1b21a58e0ab0a0
Herbert <herbert.beraldo@gmail.com>
2017-05-04 00:17:17 +0200

    Cleanup.

--- Maya/api_utils.py
+++ Maya/api_utils.py
@@ -5,13 +5,13 @@ def get_available_function_sets(mobject):
     dummy_object = OpenMaya.MFn()
     class_variables = [attr for attr in dir(dummy_object) if not callable(getattr(dummy_object, attr)) and not attr.startswith ("__") and not attr.startswith("_")]
     class_variables.remove("nodeType")
-    class_variables.remove("this")  
+    class_variables.remove("this")
     function_set_dict = {name: getattr(dummy_object, name) for name in class_variables}
-   
+
     # Get functions set for object.
     result_list =  []
     for key in function_set_dict.keys():
         if mobject.hasFn(function_set_dict[key]):
             result_list.append(key)
-   
+
     return result_list
