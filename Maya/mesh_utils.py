9a0dd5761da7a5019daee1abc5cdacf6dac93f31
Herbert <herbert.beraldo@gmail.com>
2017-05-04 15:43:22 +0200

    Pep8 en pylint cleanup.

--- Maya/mesh_utils.py
+++ Maya/mesh_utils.py
@@ -2,12 +2,12 @@ import maya.cmds as cmds
 import maya.OpenMaya as OpenMaya
 
 
-def get_vtx_position(shape_node) : 
-    vtx_world_positions = [] # [ [index, x, y, z], ... ]
-    vtx_index_list = cmds.getAttr(shape_node+".vrts", multiIndices=True)
- 
+def get_vtx_position(shape_node):
+    vtx_world_positions = []  # [ [index, x, y, z], ... ]
+    vtx_index_list = cmds.getAttr(shape_node + ".vrts", multiIndices=True)
+
     for i in vtx_index_list:
-        point_position = cmds.xform(str(shape_node)+".pnts["+str(i)+"]", query=True, translation=True, worldSpace=True)
+        point_position = cmds.xform(str(shape_node) + ".pnts[" + str(i) + "]", query=True, translation=True, worldSpace=True)
         vtx_world_positions.append(point_position)
- 
+
     return vtx_world_positions
