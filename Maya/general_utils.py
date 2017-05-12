a66ad8eb223da284e70e66c831a7d96b553edd26
hb-FA37 <hb-FA37@users.noreply.github.com>
2017-05-12 14:51:32 +0200

    Added default shelve loading method.

--- Maya/general_utils.py
+++ Maya/general_utils.py
@@ -15,11 +15,37 @@ def install_packages(packages):
     #     else:
     #         print("easy_setup installation failed, cannot install packages.")
     #         return
-    # Above doesn't work, opens a new maya instance for some reason
-    # call the mayapy.exe with the ez_setup.py file as argument instead
-
+    # Above doesn't work, opens a new Maya instance for some reason.
+    # Call the mayapy.exe with the ez_setup.py file as argument instead.
     from setuptools.command import easy_install
     if isinstance(packages, list):
         easy_install.main(packages)
     else:
         easy_install.main([packages])
+
+
+def load_default_shelves2016():
+    shelves = ["shelf_Animation", "shelf_CurvesSurfaces", "shelf_FX",
+           "shelf_FXCaching", "shelf_Polygons", "shelf_Rendering",
+           "shelf_Rigging", "shelf_Sculpting", "shelf_XGen"]
+    _load_shelves(shelves)
+
+
+def load_default_shelves2017():
+    shelves = ["shelf_Animation", "shelf_Arnold", "shelf_Bifrost",
+               "shelf_CurvesSurfaces", "shelf_FX", "shelf_FXCaching", 
+               "shelf_MASH", "shelf_MotionGraphics", "shelf_Polygons", 
+               "shelf_Rendering", "shelf_Rigging", "shelf_Sculpting", 
+               "shelf_TURTLE", "shelf_XGen"]
+    _load_shelves(shelves)
+
+
+def _load_shelves(shelves):   
+    for shelf in shelves:
+        try:
+            mel.eval('loadNewShelf "{}.mel";'.format(shelf))
+        except RuntimeError, e:
+            print "Cannot load {}".format(shelf)
+            print e
+    
+    mel.eval('saveAllShelves $gShelfTopLevel;')
